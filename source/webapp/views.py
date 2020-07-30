a55b45b69f00fecf290805ad251eeea95d085ee0
Arthur <arturkrmnlv10@gmail.com>
2020-07-30 21:02:38 +0600

    Добавлены html файлы, добавлен url адрес и представление главной страницы.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Product
+from django.http import HttpResponseNotAllowed
+# from webapp.forms import ProductForm
 
-# Create your views here.
+
+def index_view(request):
+    data = Product.objects.all()
+    return render(request, 'index.html', context={
+        'product': data
+    })
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', context={
+        'product': product})
