5e072f4d7c49b7e4c36f15dc7d2844a274b41b8f
Arthur <arturkrmnlv10@gmail.com>
2020-07-31 13:38:26 +0600

    Отредактирована форма, добалено минимальное значения для поля остатка.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -6,5 +6,5 @@ class ProductForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label='Название:')
     description = forms.CharField(max_length=2000, required=False, label='Описание товара:', widget=forms.Textarea)
     category = forms.ChoiceField(choices=CATEGORY_CHOICES, label='Категория:', initial=DEFAULT_CATEGORY)
-    amount = forms.IntegerField(label='Остаток:', required=True)
+    amount = forms.IntegerField(label='Остаток:', min_value=0, required=True)
     price = forms.DecimalField(label='Цена', required=True, max_digits=7, decimal_places=2)
