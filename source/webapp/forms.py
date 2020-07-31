a21a51ec35bd59dce8101a4ea13a64a949f17469
Arthur <arturkrmnlv10@gmail.com>
2020-07-31 14:28:10 +0600

    Отредактирована форма, добалены виджеты для стилизации полей ввода. Добавлены базовые стили.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,8 +3,13 @@ from .models import CATEGORY_CHOICES, DEFAULT_CATEGORY
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label='Название:')
-    description = forms.CharField(max_length=2000, required=False, label='Описание товара:', widget=forms.Textarea)
-    category = forms.ChoiceField(choices=CATEGORY_CHOICES, label='Категория:', initial=DEFAULT_CATEGORY)
-    amount = forms.IntegerField(label='Остаток:', min_value=0, required=True)
-    price = forms.DecimalField(label='Цена', required=True, max_digits=7, decimal_places=2)
+    name = forms.CharField(max_length=100, required=True, label='Название:',
+                           widget=forms.TextInput(attrs={'class': 'form-input'}))
+    description = forms.CharField(max_length=2000, required=False, label='Описание товара:',
+                                  widget=forms.Textarea(attrs={'class': 'form-area'}))
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, label='Категория:', initial=DEFAULT_CATEGORY,
+                                 widget=forms.Select(attrs={'class': 'form-select'}))
+    amount = forms.IntegerField(label='Остаток:', min_value=0, required=True,
+                                widget=forms.NumberInput(attrs={'class': 'form-input'}))
+    price = forms.DecimalField(label='Цена', required=True, max_digits=7, decimal_places=2,
+                               widget=forms.NumberInput(attrs={'class': 'form-input'}))
