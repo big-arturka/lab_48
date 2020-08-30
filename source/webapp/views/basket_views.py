435ed7ab1ce43797774e92bc517ac6962d0c2744
Arthur <arturkrmnlv10@gmail.com>
2020-08-30 20:23:58 +0600

    Исправлена страница просмотра корзины, отредактированы представления.

--- source/webapp/views/basket_views.py
+++ source/webapp/views/basket_views.py
@@ -8,18 +8,36 @@ from webapp.models import Basket, Product
 class ProductAddView(View):
     def post(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
-        basket = Basket.objects.filter(product=product.pk)
-        if basket:
-            cnt = basket.count() + 1
-            print(basket.count())
-            print(basket)
-            basket.update(count=cnt)
+        if Basket.objects.filter(product=product):
+            basket = Basket.objects.get(product_id=pk)
+            if basket.count < product.amount:
+                basket.count += 1
+                basket.save()
+                print('Добавил')
+            else:
+                print('Закончились')
         else:
-            Basket.objects.create(product=product, count=1)
+            if product.amount > 0:
+                Basket.objects.create(product=product, count=1)
+                print('Создал')
+            else:
+                print('Нету в наличии')
         return redirect('index')
 
 
 class BasketView(ListView):
     template_name = 'basket/basket_view.html'
     model = Basket
-    context_object_name = 'basket'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        basket_dict = {}
+        products = Basket.objects.all()
+        total = 0
+        for product in products:
+            summ = product.product.price * product.product
+            basket_dict[product.product] = summ
+            total += summ
+        context['basket'] = basket_dict
+        context['total'] = total
+        return context
