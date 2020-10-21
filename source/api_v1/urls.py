7b9f4538076b96e40afb968737d9840a75e192a6
big-smoke <arturkrmnlv10@gmail.com>
2020-10-21 14:05:27 +0600

    Создан модельный сериализатор для заказа. Создано представление просмотра заказа.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api_v1.views import get_token_view, ProductViewSet
+from api_v1.views import get_token_view, ProductViewSet, OrderListView, OrderCreateView
 
 app_name = 'api_v1'
 
@@ -11,5 +11,7 @@ router.register(r'product', ProductViewSet)
 urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
     path('', include(router.urls)),
+    path('orders/', OrderListView.as_view(), name='order_list'),
+    path('orders/create', OrderCreateView.as_view(), name='order_create'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponseNotAllowed, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -5,8 +7,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
 
-from api_v1.serializers import ProductSerializer
-from webapp.models import Product
+from api_v1.serializers import ProductSerializer, OrderSerializer
+from webapp.models import Product, Order
 
 
 @ensure_csrf_cookie
@@ -19,3 +21,20 @@ def get_token_view(request, *args, **kwargs):
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class OrderListView(APIView):
+    def get(self, request, *args, **kwargs):
+        order = Order.objects.all()
+        slr = OrderSerializer(order, many=True)
+        return Response(slr.data)
+
+
+class OrderCreateView(APIView):
+    def post(self, request, *args, **kwargs):
+        slr = OrderSerializer(data=request.data)
+        if slr.is_valid():
+            order = slr.save()
+            return Response(slr.data)
+        else:
+            return Response(slr.errors, status=400)
