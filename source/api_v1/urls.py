a2fb6708b5fe52ff1599ca190e5f2e24cd8cdb23
big-smoke <arturkrmnlv10@gmail.com>
2020-10-21 12:07:49 +0600

    Добавлено приложение API. Установлен DRF.

---
+++ source/api_v1/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api_v1.views import get_token_view, ProductViewSet
+
+app_name = 'api_v1'
+
+router = DefaultRouter()
+router.register(r'product', ProductViewSet)
+
+urlpatterns = [
+    path('get-token/', get_token_view, name='get_token'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
