fa467668049f95c333e10ead3b6664f3cee624ce
big-smoke <arturkrmnlv10@gmail.com>
2020-10-22 17:52:00 +0600

    Ура, получилось! Исправлены ошибки.

--- source/api_v1/serializers.py
+++ source/api_v1/serializers.py
@@ -9,43 +9,45 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderProductSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
     class Meta:
         model = OrderProduct
-        fields = ['id', 'product', 'qty']
+        fields = ['product', 'qty']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    products = OrderProductSerializer(many=True, read_only=True)
+    order_products = OrderProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         # fields = '__all__'
-        fields = ['id', 'name', 'phone', 'address', 'created_at', 'user', 'products']
+        fields = ['id', 'name', 'phone', 'address', 'created_at', 'user', 'order_products']
         read_only_fields = ['products']
 
-    def create(self, validated_data, context=None):
-        print(validated_data)
-        order = Order.objects.create()
-        order.name = validated_data['name']
-        order.phone = validated_data['phone']
-        order.address = validated_data['address']
-        order.user_id = validated_data['user']
-        order.save()
-        cart_products = Cart.objects.all()
-        products = []
-        order_products = []
-        for item in cart_products:
-            product = item.product
-            qty = item.qty
-            product.amount -= qty
-            products.append(product)
-            order_product = OrderProduct.objects.create(order=order, product=product, qty=qty)
-            order_products.append(order_product)
-        OrderProduct.objects.bulk_create(order_products)
-        Product.objects.bulk_update(products, ('amount',))
-        # cart_products.delete()
-        return order
+    # def create(self, validated_data):
+    #     order = Order.objects.create()
+    #     order.name = validated_data['name']
+    #     order.phone = validated_data['phone']
+    #     order.address = validated_data['address']
+    #     order.user_id = validated_data['user']
+    #     # order.save()
+    #     cart_products = Cart.objects.all()
+    #     products = []
+    #     order_products = []
+    #     for item in cart_products:
+    #         product = item.product
+    #         qty = item.qty
+    #         product.amount -= qty
+    #         products.append(product)
+    #         order_product = OrderProduct.objects.create(order=order, product=product, qty=qty)
+    #         order_products.append(order_product)
+    #     OrderProduct.objects.bulk_create(order_products)
+    #     Product.objects.bulk_update(products, ('amount',))
+    #     cart_products.delete()
+    #     return order
 
+#
 # {
 #         "name": "test",
 #         "phone": "123",
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -41,27 +41,24 @@ class OrderViewSet(ViewSet):
 
     def list(self, request):
         objects = Order.objects.all()
-        slr = OrderSerializer(objects, many=True, context={'request': request})
+        slr = OrderSerializer(objects, many=True)
         return Response(slr.data)
 
     def create(self, request):
-        slr = OrderSerializer(data=request.data, context={'request': request})
-        order = slr.create(request.data, context={})
+        slr = OrderSerializer(data=request.data)
         if slr.is_valid():
-            order.save()
-            ans = {
-                "id": order.id,
-                "name": order.name,
-                "phone": order.phone,
-                "address": order.address,
-                "created_at": order.created_at
-            }
-            return Response(ans)
+            order = slr.save()
+            cart_ids = self.request.session.get('cart_ids', [])
+            for i in cart_ids:
+                cart = get_object_or_404(Cart, pk=i)
+                OrderProduct.objects.create(product_id=cart.product_id, order_id=order.pk, qty=cart.qty)
+                cart.delete()
+            self.request.session.pop('cart_ids')
+            return Response(slr.data)
         else:
             return Response(slr.errors, status=400)
 
     def retrieve(self, request, pk=None):
         object = get_object_or_404(Order, pk=pk)
-        slr = OrderSerializer(object, context={'request': request})
+        slr = OrderSerializer(object)
         return Response(slr.data)
-
